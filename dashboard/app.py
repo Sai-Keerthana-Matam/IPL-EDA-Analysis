9193006e6c60d9dcebfd80bccd1c73fdd7fb2cbe
Sai Keerthana <msk.civil2006@gmail.com>
2026-06-15 18:04:57 +0530

    Completed IPL EDA Dashboard Project

---
+++ dashboard/app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+
+# Page title
+st.title("🏏 IPL Data Analysis Dashboard")
+
+# Load data
+matches = pd.read_csv("../data/matches.csv")
+deliveries = pd.read_csv("../data/deliveries.csv")
+
+# Dataset preview
+st.header("Dataset Preview")
+
+st.subheader("Matches Dataset")
+st.dataframe(matches.head())
+
+st.subheader("Deliveries Dataset")
+st.dataframe(deliveries.head())
+st.header("Top IPL Teams")
+
+team_wins = matches['winner'].value_counts()
+
+st.bar_chart(team_wins)
+st.header("Top Player of the Match Winners")
+
+top_players = matches['player_of_match'].value_counts().head(10)
+
+st.bar_chart(top_players)
+st.header("Top Run Scorers")
+
+top_batsmen = deliveries.groupby(
+    'batter'
+)['batsman_runs'].sum().sort_values(
+    ascending=False
+).head(10)
+
+st.bar_chart(top_batsmen)
+st.header("Top Wicket Takers")
+
+top_bowlers = deliveries[
+    deliveries['player_dismissed'].notna()
+]['bowler'].value_counts().head(10)
+
+st.bar_chart(top_bowlers)
+st.header("Top IPL Venues")
+
+venues = matches['venue'].value_counts().head(10)
+
+st.bar_chart(venues)
